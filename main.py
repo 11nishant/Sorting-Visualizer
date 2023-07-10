1bd1b229c0d502d6dcf3879e3e1db03a66e487a1
Anshul Mittal <112956069+Anshul091@users.noreply.github.com>
2023-07-11 01:56:07 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from sorting_algorithms import BubbleSort, MergeSort, InsertionSort, QuickSort
+import sys
+import random
+
+# when this file is itself is ran, not by importing.
+if __name__ == '__main__':
+	ms_speed = 180  #interval in millisec by which animation will run.
+	li = []
+	step_cnt = 0 
