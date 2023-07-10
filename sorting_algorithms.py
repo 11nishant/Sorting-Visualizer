24ffb9c1ba90bff9c16e267d60e35af2d3e5ea20
Aditya Kumar Sahu <adityak25092002@gmail.com>
2023-07-11 01:56:41 +0530

    added swap

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,4 +1,9 @@
-'''The yield statement suspends function’s execution and sends a value back to the caller, but retains 
+'''The yield statement suspends functions execution and sends a value back to the caller, but retains 
   	enough state to enable function to resume where it is left off. When resumed, the function continues 
   	execution immediately after the last yield run. This allows its code to produce a series of values over time, 
   	rather than computing them at once '''
+
+
+def swap(li, i, j):
+	if i != j:
+		li[i], li[j] = li[j], li[i]
